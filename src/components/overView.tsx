6b5f9e933d4b40b46483a02e5723dfd3d2b57376
Adewale Orotayo <didymus7007@gmail.com>
2025-11-03 02:48:36 +0100

    - overview card added

---
+++ src/components/overView.tsx
@@ -0,0 +1,6 @@
+import { Card } from "./primitives/card";
+import { Clock } from "./primitives/icons";
+
+export const Overview = () => {
+  return <Card icon={<Clock />} title={"Sessions Overview"} />;
+};
