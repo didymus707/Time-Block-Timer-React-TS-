2cbc987c65b3c9788132aca934364021d73c67c1
Adewale Orotayo <didymus7007@gmail.com>
2025-11-06 10:53:56 +0100

    - wrap Routes with BlockProvider for context management

--- src/main.tsx
+++ src/main.tsx
@@ -4,17 +4,20 @@ import { StrictMode } from "react";
 import { BrowserRouter, Route, Routes } from "react-router";
 import { createRoot } from "react-dom/client";
 import { CardDetails } from "./pages/blockCardDetail.tsx";
+import { BlockProvider } from "./context/blockContextPRovider.tsx";
 
 const root = document.getElementById("root")!;
 
 createRoot(root).render(
   <StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}>
-          <Route path="/block/:id" element={<CardDetails />} />
-        </Route>
-      </Routes>
+      <BlockProvider>
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route path="/block/:id" element={<CardDetails />} />
+          </Route>
+        </Routes>
+      </BlockProvider>
     </BrowserRouter>
   </StrictMode>
 );
