56cec56f1383cfb5c8e17887ce1b244196797c5f
Adewale Orotayo <didymus7007@gmail.com>
2025-11-06 13:48:18 +0100

    feat: initialize BlockContext with default values and add useBlock hook

--- src/context/blockContext.tsx
+++ src/context/blockContext.tsx
@@ -1,4 +1,9 @@
-import { createContext } from "react";
+import { createContext, useContext } from "react";
 import type { BlockContextType } from "../types";
 
-export const BlockContext = createContext<BlockContextType | null>(null);
+export const BlockContext = createContext<BlockContextType>({
+  blocks: [],
+  dispatch: () => {},
+});
+
+export const useBlock = () => useContext(BlockContext);
