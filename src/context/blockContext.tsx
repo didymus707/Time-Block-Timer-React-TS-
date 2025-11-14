0f7bd90008967dd711ff077683ff265d28f7a8ef
Adewale Orotayo <didymus7007@gmail.com>
2025-11-14 10:52:45 +0100

    feat: add BlockUIContext and BlockUIProvider for managing modal state

--- src/context/blockContext.tsx
+++ src/context/blockContext.tsx
@@ -1,9 +1,20 @@
 import { createContext, useContext } from "react";
-import type { BlockContextType } from "../types";
+import type { BlockContextType, BlockUIContextType } from "../types";
 
 export const BlockContext = createContext<BlockContextType>({
   blocks: [],
   dispatch: () => {},
 });
 
+export const BlockUIContext = createContext<BlockUIContextType | undefined>(
+  undefined
+);
+
 export const useBlock = () => useContext(BlockContext);
+
+export const useBlockUI = () => {
+  const context = useContext(BlockUIContext);
+  if (!context)
+    throw new Error("useBlockUI must be used within BlockUIProvider");
+  return context;
+};
--- src/context/blockContextProvider.tsx
+++ src/context/blockContextProvider.tsx
@@ -1,11 +1,11 @@
-import { useReducer } from "react";
-import { BlockContext } from "./blockContext";
+import { useReducer, useState, type ReactNode } from "react";
+import { BlockContext, BlockUIContext } from "./blockContext";
 import { blockReducer } from "../reducers/reducer";
 import type { Block, BlockContextType } from "../types";
 
 const initialState: Block[] = [];
 
-export const BlockProvider = ({ children }: { children: React.ReactNode }) => {
+export const BlockProvider = ({ children }: { children: ReactNode }) => {
   const [blocks, dispatch] = useReducer(blockReducer, initialState);
   return (
     <BlockContext.Provider value={{ blocks, dispatch } as BlockContextType}>
@@ -13,4 +13,17 @@ export const BlockProvider = ({ children }: { children: React.ReactNode }) => {
     </BlockContext.Provider>
   );
 };
+
+export const BlockUIProvider = ({children}: {children: ReactNode}) => {
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
+
+  return (
+    <BlockUIContext.Provider value={{isModalOpen, openModal, closeModal}}>
+      {children}
+    </BlockUIContext.Provider>
+  );
+}
 
