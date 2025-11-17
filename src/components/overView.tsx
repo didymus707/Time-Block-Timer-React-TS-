fb886758b46557c74f4e95a9edef159969a4b210
Adewale Orotayo <didymus7007@gmail.com>
2025-11-17 15:27:15 +0100

    feat: enhance Overview component with detailed session statistics

--- src/components/overView.tsx
+++ src/components/overView.tsx
@@ -1,6 +1,77 @@
+import { useBlock } from "../context/blockContext";
 import { Card } from "./primitives/card";
-import { Clock } from "./primitives/icons";
+import { CheckCircle, Clock, Pause, Play, Task } from "./primitives/icons";
+import type { ReactNode } from "react";
 
 export const Overview = () => {
-  return <Card icon={<Clock />} title={"Sessions Overview"} />;
+  const { blocks } = useBlock();
+
+  const total = blocks.length;
+  const paused = blocks.filter((b) => b.status === "paused").length;
+  const running = blocks.filter((b) => b.status === "running").length;
+  const completed = blocks.filter((b) => b.status === "completed").length;
+  const tasksCompleted = blocks.reduce(
+    (acc, block) => acc + block.tasks.filter((t) => t.completed).length,
+    0
+  );
+  const totalTasks = blocks.reduce((acc, block) => acc + block.tasks.length, 0);
+
+  return (
+    <>
+      <Card
+        className="bg-transparent mb-8 p-6 border border-gray-200 shadow-none "
+        title="Sessions Overview"
+        icon={<Clock color="black" classNames={["mr-2"]} />}
+      >
+        <div className="space-y-8">
+          <div className="grid grid-cols-2 md:grid-cols-5 gap-4">
+            <StatBox
+              label="Completed"
+              value={completed}
+              icon={<CheckCircle color="green" />}
+            />
+            <StatBox
+              label="Running"
+              value={running}
+              icon={<Play color="blue" />}
+            />
+            <StatBox
+              label="Paused"
+              value={paused}
+              icon={<Pause color="orange" />}
+            />
+            <StatBox
+              label="Total Sessions"
+              value={total}
+              icon={<Clock color="gray" />}
+            />
+            <StatBox
+              label="Total Sessions"
+              icon={<Task color="purple" />}
+              value={tasksCompleted + " / " + totalTasks}
+            />
+            {/*   */}
+          </div>
+        </div>
+      </Card>
+    </>
+  );
+};
+
+const StatBox = ({
+  label,
+  value,
+  icon,
+}: {
+  label: string;
+  value: string | number;
+  icon?: ReactNode;
+}) => {
+  return (
+    <div className="bg-gray-100 p-4 rounded-lg flex flex-col items-center justify-center space-y-1">
+      <span className="text-2xl">{icon}</span>
+      <span className="text-xl font-bold text-gray-900 py-2">{value}</span>
+      <span className="text-sm text-gray-600">{label}</span>
+    </div>
+  );
 };
